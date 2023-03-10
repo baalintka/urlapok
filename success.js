95b2b664d0eea9d35f89a0ecd05f23471d7f14ad
Szöllősi Bálint (SZF_2022_2024) <ballint3@gmail.com>
2023-03-10 11:01:07 +0100

    first commiaat

---
+++ success.js
@@ -0,0 +1,27 @@
+
+		const form = document.querySelector('form');
+		const successMessage = document.querySelector('.success-message');
+
+		form.addEventListener('submit', function(event) {
+			event.preventDefault(); // prevent the form from submitting normally
+
+			// send the form data to the server using AJAX
+			const xhr = new XMLHttpRequest();
+			xhr.open('GET', 'action.php');
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4 && xhr.status === 200) {
+					// show the success message if the form submission was successful
+					successMessage.style.display = 'block';
+				}
+			};
+			xhr.send(new FormData(form));
+		});
+		function validateForm() {
+			let x = document.validTeszt.kNev.value;
+			if (x === "") {
+				alert("A mező kitöltése kötelező!");
+				return false;
+			}
+		}
+		
